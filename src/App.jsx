3171eb97a484ecff05983c92a80536aee60f34df
matiasecharri <matiasecharri@hotmail.com>
2024-05-11 22:17:27 -0300

    feat: added params and formater to card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import TwitterFollowCard from "./TwitterFollowCard";
+import TwitterFollowCard from "./components/TwitterFollowCard";
 
 const App = () => {
+  const addAt = formatedName => `@${formatedName}`;
+
   return (
-    <>
-      <TwitterFollowCard />
-      <TwitterFollowCard />
-      <TwitterFollowCard />
-      <TwitterFollowCard />
-      <TwitterFollowCard />
-    </>
+    <section className="App">
+      <TwitterFollowCard
+        format={addAt}
+        isFollowing
+        userName={"matiasecharri"}
+        name={"Matias Echarri"}
+      />
+      <TwitterFollowCard
+        format={addAt}
+        isFollowing
+        userName={"dianavodopivec"}
+        name={"Diana Vodopivec"}
+      />
+      <TwitterFollowCard
+        format={addAt}
+        isFollowing={false}
+        userName={"pachupacha"}
+        name={"Gabriel García"}
+      />
+    </section>
   );
 };
 
