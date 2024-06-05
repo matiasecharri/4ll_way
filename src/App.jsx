ca25e2ec8d726f14e38a52f8c5caac240ce9d3bd
matiasecharri <matiasecharri@hotmail.com>
2024-06-05 18:11:56 -0300

    build: T.T.T

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Counter from "./components/Counter/Counter";
 import ContainerCards from "./components/TwitterFollowCard/ContainerCards";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
 
 const App = () => {
   return (
     <>
       <Counter></Counter>
       <ContainerCards></ContainerCards>
+      <TicTacToe></TicTacToe>
     </>
   );
 };
---
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,11 @@
+import "/src/components/TicTacToe/TicTacToe.css";
+
+const TicTacToe = () => {
+  return (
+    <div>
+      <p>TicTacToe</p>
+    </div>
+  );
+};
+
+export default TicTacToe;
